6b058f8a1d3ff58bc496e0cc2c19842ff2325c09
Hyy <15999966670@163.com>
2019-12-24 21:49:56 +0800

    2019年12月24日

--- spring-core/src/main/java/org/springframework/core/io/WritableResource.java
+++ spring-core/src/main/java/org/springframework/core/io/WritableResource.java
@@ -32,37 +32,19 @@ import java.nio.channels.WritableByteChannel;
 public interface WritableResource extends Resource {
 
 	/**
-	 * Indicate whether the contents of this resource can be written
-	 * via {@link #getOutputStream()}.
-	 * <p>Will be {@code true} for typical resource descriptors;
-	 * note that actual content writing may still fail when attempted.
-	 * However, a value of {@code false} is a definitive indication
-	 * that the resource content cannot be modified.
-	 * @see #getOutputStream()
-	 * @see #isReadable()
+	 * 资源是否可写
 	 */
 	default boolean isWritable() {
 		return true;
 	}
 
 	/**
-	 * Return an {@link OutputStream} for the underlying resource,
-	 * allowing to (over-)write its content.
-	 * @throws IOException if the stream could not be opened
-	 * @see #getInputStream()
+	 * 获取资源 OutputStream
 	 */
 	OutputStream getOutputStream() throws IOException;
 
 	/**
-	 * Return a {@link WritableByteChannel}.
-	 * <p>It is expected that each call creates a <i>fresh</i> channel.
-	 * <p>The default implementation returns {@link Channels#newChannel(OutputStream)}
-	 * with the result of {@link #getOutputStream()}.
-	 * @return the byte channel for the underlying resource (must not be {@code null})
-	 * @throws java.io.FileNotFoundException if the underlying resource doesn't exist
-	 * @throws IOException if the content channel could not be opened
-	 * @since 5.0
-	 * @see #getOutputStream()
+	 * 返回资源 WritableByteChannel
 	 */
 	default WritableByteChannel writableChannel() throws IOException {
 		return Channels.newChannel(getOutputStream());
